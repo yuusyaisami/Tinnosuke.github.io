0ccd211b8780125c81f576d7512d84ac152b82a7
YuusyaIsami <macaronkunkun@gmail.com>
2026-05-13 08:13:49 +0900

    修正

--- src/config.ts
+++ src/config.ts
@@ -12,13 +12,18 @@ export type SiteConfig = {
   aboutMe: string;
   skills: string[];
   hero_title: string;
+  title_image: string;
+  thanks: string;
 };
 
 export const siteConfig: SiteConfig = {
   name: "Tinnosuke",
-  title: "Tinnosuke's Portfolio",
+  title: "Portfolio",
+  title_image: "/Sprite/Title_Image1.png",
   hero_title: "Welcome to my portfolio!",
   description: "Portfolio website of Tinnosuke",
+
+  thanks: "ご興味持っていただきありがとうございます！",
   accentColor: "#1d4ed8",
   social: {
     email: "gotfather.tanakamichael@gmail.com",
@@ -26,5 +31,7 @@ export const siteConfig: SiteConfig = {
   },
   aboutMe:
     "Unity/C#を用いたゲーム開発を個人で継続的に行っています。現在はゲーム制作と並行して、拡張性・保守性・パフォーマンスを重視したUnity向けゲームフレームワークの開発にも取り組んでいます。特に、アーキテクチャ設計、Editor拡張、Shader・描画表現などに興味があり、UI Toolkitを用いたツール制作や、URP環境での描画・演出表現について学習しています。個人開発では、企画から実装、UI、調整まで一通り担当しており、短期間でのプロトタイプ制作も継続的に行っています。",
-  skills: ["Unity", "C#", "Shader"],
-};
+  skills: [
+    "Unity",
+    "C#",]
+  };
